ec89a3dbd87bf10170aad5df8b18b22519758f64
Tom <wiredzenith@gmail.com>
2017-11-20 21:58:34 +0000

    tidy up

---
+++ mySQL_add/defines.h
@@ -0,0 +1,5 @@
+#define CONNECTOR     "rest"
+#define SERVER_ADDR   "192.168.1.4"
+
+Adafruit_HTU21DF htu = Adafruit_HTU21DF();
+Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
--- mySQL_add/mySQL_add.ino
+++ mySQL_add/mySQL_add.ino
@@ -1,27 +1,13 @@
-#include <Adafruit_Sensor.h>
-#include <Adafruit_TSL2561_U.h>
-#include <pgmspace.h>
-#include <Wire.h>
-#include <UnoWiFiDevEd.h>
-#include <Adafruit_HTU21DF.h>
-
-#define CONNECTOR     "rest"
-#define SERVER_ADDR   "192.168.1.101"
-
-Adafruit_HTU21DF htu = Adafruit_HTU21DF();
-Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
+#include <includes.h>
+#include <defines.h>
 
 unsigned long previousMillis = 0;
-
-
 
 void setup()
 {
   Ciao.begin();
   Serial.begin(9600);
   configureSensor();
-
-
 }
 
 void loop()
