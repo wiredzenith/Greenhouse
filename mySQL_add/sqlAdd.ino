7c29abafaba7fc243d562b4d94680f1c2e06d6bd
Tom <wiredzenith@gmail.com>
2017-11-21 11:47:48 +0000

    merge

--- mySQL_add/sqlAdd.ino
+++ mySQL_add/sqlAdd.ino
@@ -1,3 +1,4 @@
+<<<<<<< current
 //Function for readding from sensors and sending them to MySQL DB
 //
 //Tomasz Klebek
@@ -24,3 +25,33 @@ String uri = "/project/write_data.php?value=";
 CiaoData data = Ciao.write(CONNECTOR, SERVER_ADDR, uri);
 delay(10000);
 }
+=======
+//Function for readingin values from sensors and sending them to
+//MySQL DB.
+//
+//Tomasz Klebek
+//2017/11/21
+
+void sqlAdd()
+{
+sensors_event_t event;
+tsl.getEvent(&event);
+
+Serial.println(htu.readTemperature());
+Serial.println(soilMoisture());
+
+String uri = "/php/add.php?value=";
+       uri += String(htu.readTemperature());
+       uri += ",";
+       uri += String(htu.readHumidity());
+       uri += ",";
+       uri += String(event.light);
+      //  uri += "value4=";
+      //  uri += String(htu.readTemperature());
+
+
+
+CiaoData data = Ciao.write(CONNECTOR, SERVER_ADDR, uri);
+delay(900000);
+}
+>>>>>>> before discard
