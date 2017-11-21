7b776f977beaba62c6bbf1eac31e90f103933a60
Tom <wiredzenith@gmail.com>
2017-11-21 11:50:45 +0000

    tindy and rename

--- mySQL_add/sqlAdd.ino
+++ mySQL_add/sqlAdd.ino
@@ -1,31 +1,3 @@
-<<<<<<< current
-//Function for readding from sensors and sending them to MySQL DB
-//
-//Tomasz Klebek
-//2017/11/21
-void sqlAdd()
-{
-sensors_event_t event;
-tsl.getEvent(&event);
-
-Serial.println(htu.readTemperature());
-Serial.println(soilMoisture());
-
-String uri = "/project/write_data.php?value=";
-       uri += String(htu.readTemperature());
-       uri += ",";
-       uri += String(htu.readHumidity());
-       uri += ",";
-       uri += String(htu.readTemperature());
-      //  uri += "value4=";
-      //  uri += String(htu.readTemperature());
-
-
-
-CiaoData data = Ciao.write(CONNECTOR, SERVER_ADDR, uri);
-delay(10000);
-}
-=======
 //Function for readingin values from sensors and sending them to
 //MySQL DB.
 //
@@ -54,4 +26,3 @@ String uri = "/php/add.php?value=";
 CiaoData data = Ciao.write(CONNECTOR, SERVER_ADDR, uri);
 delay(900000);
 }
->>>>>>> before discard
